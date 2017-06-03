5be9ae0e46ce6cc1f724dd4007d4d7907f467b4c
Natalia <mazurnata84@gmail.com>
2017-06-03 14:07:24 +0300

    changed variable's name

---
+++ src/javacourse7/homework/module1/ArraysDemo.java
@@ -0,0 +1,29 @@
+package javacourse7.homework.module1;
+
+import java.util.*;
+
+public class ArraysDemo {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter any number ");
+        int size = input.nextInt();
+        int array[] = new int[size];
+        for (int i = 0; i < size; i++) {
+            System.out.print("Enter number " + (i + 1) + ": ");
+            array[i] = input.nextInt();
+        }
+
+        System.out.print("Initial array:");
+        for (int i = 0; i < size; i++) {
+            System.out.print(" " + array[i]);
+        }
+
+        System.out.println();
+        Arrays.sort(array);
+        System.out.print("Sorted array:");
+        for (int el: array ) {
+            System.out.print(" " + el);
+        }
+    }
+}

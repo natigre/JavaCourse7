c232594b9e668fd57577a4fa4daaaff5e5256daf
Natalia <mazurnata84@gmail.com>
2017-06-03 11:18:09 +0300

    first commit

---
+++ src/javacourse7/module1/homework/Greeting.java
@@ -0,0 +1,17 @@
+package javacourse7.module1.homework;
+
+import java.util.*;
+
+public class Greeting {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter your name: ");
+        String name = in.nextLine();
+
+        System.out.println("Hello, " + name);
+
+
+    }
+}
